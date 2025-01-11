efbdefd97a2efb7089914b164176d16607e5446e
ilai dabush <ilai.dabush@gmail.com>
2025-01-11 17:01:22 +0200

    feat: added dependencies, set env by OS

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import platform
+import os
+
+def set_env():
+    os.environ["SUMO_HOME"] = r"C:\Program Files (x86)\Eclipse\Sumo" if platform.system() == "Windows" else "/usr/share/sumo"
+    os.environ["LIBSUMO_AS_TRACI"] = "1"
+    tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+    sys.path.append(tools)
+
+
+
+if __name__ == "__main__":
+    set_env()
+    print(os.environ["SUMO_HOME"], os.environ["LIBSUMO_AS_TRACI"])
