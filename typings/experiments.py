59d0aa17a1788f8e577d099c3be7488a7e5d5348
ilai dabush <ilai.dabush@gmail.com>
2025-01-12 23:08:33 +0200

    feat: typed config

--- typings/experiments.py
+++ typings/experiments.py
@@ -1,13 +1,47 @@
-from typing import Annotated, Optional, List
+from typing import Annotated, Optional, List, TypeAlias, Literal, Generic, TypeVar, Union
 
 from pydantic import BaseModel, Field, PositiveInt, PositiveFloat
+
+T = TypeVar("T")
 
 
 class ExperimentBaseConfig(BaseModel):
     train_batch_size: PositiveInt
     lr: PositiveFloat
     gamma: PositiveFloat
-    grad_clip: Optional[PositiveFloat]
+    grad_clip: Optional[PositiveFloat] = None
+
+
+class BasePPOExperimentConfig(ExperimentBaseConfig):
+    clip_param: PositiveFloat
+    vf_loss_coeff: Optional[PositiveFloat] = None
+    entropy_coeff: Optional[PositiveFloat] = None
+    use_gae: bool
+    use_critic: bool
+    lambda_: PositiveFloat
+
+
+class PPOExperimentConfig(BasePPOExperimentConfig):
+    algo_name: Literal["PPO"]
+    sgd_minibatch_size: PositiveInt
+    num_sgd_iter: PositiveInt
+
+
+class APPOExperimentConfig(BasePPOExperimentConfig):
+    algo_name: Literal["APPO"]
+    use_kl_loss: bool
+    kl_coeff: PositiveFloat
+    kl_target: PositiveFloat
+
+
+class DQNExperimentConfig(ExperimentBaseConfig):
+    algo_name: Literal["DDQN", "DQN"]
+    target_network_update_freq: PositiveInt
+    dueling: bool
+    double_q: bool
+    hiddens: List[PositiveInt]
+    n_step: PositiveInt
+    training_intensity: Optional[PositiveFloat] = None
     store_buffer_in_checkpoints: Annotated[bool, Field(default=False)]
     adam_epsilon: PositiveFloat
     v_max: Annotated[float, Field(default=10.0)]
@@ -17,13 +51,48 @@ class ExperimentBaseConfig(BaseModel):
     sigma0: PositiveFloat
 
 
-class DQNExperimentConfig(ExperimentBaseConfig):
-    target_network_update_freq: PositiveInt
-    dueling: bool
-    double_q: bool
-    hiddens: List[PositiveInt]
-    n_step: PositiveInt
-    training_intensity: Optional[PositiveFloat]
+ParamSpaceFunc: TypeAlias = Literal["tune.uniform", "tune.loguniform", "tune.choice", "tune.randint"]
+
+
+class ParamConfig(BaseModel, Generic[T]):
+    func: ParamSpaceFunc
+    args: List[T]
+
+
+class ParamSpaceConfig(BaseModel):
+    lr: ParamConfig[PositiveFloat]
+    gamma: ParamConfig[PositiveFloat]
+
+
+class DQNParamSpaceConfig(ParamSpaceConfig):
+    algo_name: Literal["DQN", "DDQN"]
+    target_network_update_freq: ParamConfig[List[PositiveInt]]
+    hiddens: ParamConfig[List[List[PositiveInt]]]
+    n_step: ParamConfig[List[PositiveInt]]
+    adam_epsilon: ParamConfig[PositiveFloat]
+    train_batch_size: ParamConfig[List[PositiveInt]]
+
+
+class BasePPoParamSpaceConfig(ParamSpaceConfig):
+    clip_param: ParamConfig[PositiveFloat]
+    lambda_: ParamConfig[PositiveFloat]
+    grad_clip: Optional[ParamConfig[PositiveFloat]] = None
+    lr: ParamConfig[PositiveFloat]
+    gamma: ParamConfig[PositiveFloat]
+    vf_loss_coeff: Optional[ParamConfig[PositiveFloat]]
+    entropy_coeff: Optional[ParamConfig[PositiveFloat]]
+
+
+class PPOParamSpaceConfig(BasePPoParamSpaceConfig):
+    algo_name: Literal["PPO"]
+    num_sgd_iter: ParamConfig[PositiveInt]
+    sgd_minibatch_size: Optional[ParamConfig[PositiveInt]] = None
+
+
+class APPOParamSpaceConfig(BasePPoParamSpaceConfig):
+    algo_name: Literal["APPO"]
+    kl_coeff: ParamConfig[PositiveFloat]
+    kl_target: ParamConfig[PositiveFloat]
 
 
 class Experiment(BaseModel):
@@ -33,4 +102,6 @@ class Experiment(BaseModel):
     num_of_episodes: PositiveInt
     checkpoint_freq: PositiveInt
     num_env_runners: PositiveInt
-    config: ExperimentBaseConfig
+    config: Union[DQNExperimentConfig, PPOExperimentConfig, APPOExperimentConfig] = Field(discriminator="algo_name")
+    param_space: Union[DQNParamSpaceConfig, APPOParamSpaceConfig, PPOParamSpaceConfig] = Field(
+        discriminator="algo_name")
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,16 @@
 import json
 from pathlib import Path
+from typing import List
+
+from pydantic import TypeAdapter
+
+from typings.experiments import Experiment
 
 
-def read_config(file_path: Path = Path('experiment_configurations/base_config.json')):
+def read_config(file_path: Path = Path('experiment_configurations/base_config.json')) -> List[Experiment]:
     if not file_path.exists() or not file_path.is_file():
         raise FileNotFoundError(f"File {file_path} does not exist.")
     if not str(file_path).endswith('.json'):
         raise ValueError(f"File {file_path} is not a JSON file.")
     with open(file_path, 'r') as file:
-        return json.load(file)
+        return TypeAdapter(List[Experiment]).validate_python(json.load(file)["experiments"])
