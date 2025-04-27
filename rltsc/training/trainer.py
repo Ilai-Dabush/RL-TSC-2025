c12a66d3b2dbb99dd4ff95f94dad9d2962b2d46c
ilai dabush <ilai.dabush@gmail.com>
2025-04-28 00:10:01 +0300

    feat: test

--- rltsc/training/trainer.py
+++ rltsc/training/trainer.py
@@ -34,7 +34,7 @@ class Trainer:
 
     def _bootsrap(self):
         bootstrap()
-        ray.shutdown()
+        # ray.shutdown()
         ray.init(num_cpus=4, num_gpus=self.experiment.num_gpus, ignore_reinit_error=True)
 
     def create_env(
